f8edb46943679efe3da36ce97a98cc286857c1f0
carbon <carbon24092409@gmail.com>
2023-12-22 19:38:35 +0300

    added messages func

--- users_app/views.py
+++ users_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import UserRegistrationForm, AuthenticationForm
 from .models import CustomUser
+from django.contrib import messages, auth
+
 
 class UserRegistrationView(CreateView):
     form_class = UserRegistrationForm
@@ -12,6 +14,11 @@ class UserRegistrationView(CreateView):
     success_url = reverse_lazy('users_app:login_url')
     template_name = 'users_app/register.html'
     context_object_name = 'form'
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, message='Вы успешно зарегистрировались')
+        response = super().post(request)
+        return response
 
 # def registration(request):
 #     if request.method == 'POST':
@@ -22,6 +29,7 @@ class UserRegistrationView(CreateView):
 #         return render(request, 'users_app/register.html', {'form': form})
 #     else:
 #         return render(request, 'users_app/register.html', context={'form': UserRegistrationForm()})
+
 
 class UserLoginView(LoginView):
     form_class = AuthenticationForm
