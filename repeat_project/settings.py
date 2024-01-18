b133c1bf26ecfd77ef6d374edd92783f3589f450
carbon <carbon24092409@gmail.com>
2024-01-18 05:45:35 +0300

    changed db to PostgreSql

--- repeat_project/settings.py
+++ repeat_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'repeat_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'repeat_project_psql',
+        'USER': 'carbon',
+        'PASSWORD': '197053',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
