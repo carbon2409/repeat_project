ac053ee03a8d159c5c216f09bc8eec6d129304c7
carbon <carbon24092409@gmail.com>
2024-01-29 19:48:47 +0300

    added humanize price on basket and products

--- repeat_project/settings.py
+++ repeat_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'debug_toolbar',
+    'django.contrib.humanize',
 
     'allauth.socialaccount.providers.github',
     'allauth',
